36b79bf10f67ae3f1917ab038ea09ebb790c2bd9
Francesco Paparatto <francescopaparatto@gmail.com>
2025-10-24 17:02:56 +0200

    added a function to normalize phone number (E.164) format and tested it

--- contacts.py
+++ contacts.py
@@ -16,9 +16,11 @@ from dataclasses import dataclass, field
 # TODO: in the docs, specify that I have simplified email validation and not covered all possible use cases
 EMAIL_PATTERN = r"^[\w\.]+@([\w]+\.)+[\w]{2,3}$"
 PHONE_NUMBER_PATTERN = r"^(?:\+39\s?)?3\d{2}\s?\d{3}\s?\d{4}$"
+PREFIX = "+39"
+
 
 @dataclass
-class Contact: 
+class Contact:
     phone_number: str
     first_name: Optional[str] = None
     last_name: Optional[str] = None
@@ -29,6 +31,17 @@ class Contact:
         if name:
             return name.strip().capitalize()
 
+    def _normalize_phone_number(self, phone_number: str):
+        if not re.match(PHONE_NUMBER_PATTERN, self.phone_number):
+            raise ValueError("Invalid phone number")
+
+        if not phone_number.strip().startswith(PREFIX):
+            phone_number = PREFIX + phone_number
+
+        phone_number_parts = phone_number.split()
+
+        return "".join(phone_number_parts)
+
     def get_full_name(self):
         # TODO: understand if this method is necessary or if there is a better way to do it.
         if self.first_name and self.last_name:
@@ -36,26 +49,40 @@ class Contact:
 
     def __post_init__(self):
         # TODO: understand if there is a way to handle this better
-        if self.email and not re.match(EMAIL_PATTERN, self.email):
-                raise ValueError("Invalid email format")
-
-        if not re.match(PHONE_NUMBER_PATTERN, self.phone_number):
-            raise ValueError("Invalid phone number")
-    
         if self.first_name:
             self.first_name = self._normalize_name(self.first_name)
 
         if self.last_name:
             self.last_name = self._normalize_name(self.last_name)
 
+        self.phone_number = self._normalize_phone_number(self.phone_number)
+
+        if self.email and not re.match(EMAIL_PATTERN, self.email):
+            raise ValueError("Invalid email format")
 
 
 if __name__ == "__main__":
     # I noticed that for optional parameters if I don't use positional arguments they are assigned randomly, for example 'Musk' could be assigned to the phone_number variable
-    john = Contact(first_name="John", last_name="Carmack", phone_number="+39 329 3892918", email="john@test.com")
-    sec= Contact(first_name="Elon", last_name="Musk", phone_number="+39 329 3892919", email="elon@test.com")
+    john = Contact(
+        first_name="John",
+        last_name="Carmack",
+        phone_number="+39 329 3892918",
+        email="john@test.com",
+    )
+    elon = Contact(
+        first_name="Elon",
+        last_name="Musk",
+        phone_number="+39 329 3892919",
+        email="elon@test.com",
+    )
 
+    pavel = Contact(
+        first_name="pavel  ",
+        last_name="  durov",
+        phone_number="+39 329 389 2919",
+        email="pavel@telegram.com",
+    )
 
-
-
-
+    print(pavel.first_name)
+    print(pavel.last_name)
+    print(pavel.phone_number)
